70f6a20f67657a193485b0e99f71672dc34874f9
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-01 18:00:38 +0530

    Added : Program for Calculate the daily Employee Wage

--- src/com/blz/day9/EmpWage.java
+++ src/com/blz/day9/EmpWage.java
@@ -2,27 +2,38 @@ package com.blz.day9;
 
 import java.util.Random;
 
-class utilityEmpWage{
-    Random rand = new Random();
-    public int isPresent = 1;
-    public int isAbsent = 2;
+class utilityEmpWage {
+	Random rand = new Random();
+	public int isPresent = 1;
+	public int isAbsent = 2;
+	public int wagePerHr = 20;
+	public int fullDayHr = 8;
+	int check = rand.nextInt(2) + 1;
 
-    public void present() {
-        int check = rand.nextInt(2)+1;
-        if (check == isAbsent) {
-            System.out.println("Employee is absent.");
-        }
-        else {
-            System.out.println("Employee is present.");
-        }
-    }
+	public boolean present() {
+		if (check == isAbsent) {
+			System.out.println("Employee is absent.");
+			return false;
+		} else {
+			System.out.println("Employee is present.");
+			return true;
+		}
+	}
+
+	public void dailyWage() {
+		if (present()) {
+			int dailyWage = wagePerHr * fullDayHr;
+			System.out.println("Wage of the day of employee is : " + dailyWage);
+		}
+	}
 }
+
 public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation");
 		utilityEmpWage empWage = new utilityEmpWage();
-        empWage.present();
+		empWage.present();
 	}
 
 }
