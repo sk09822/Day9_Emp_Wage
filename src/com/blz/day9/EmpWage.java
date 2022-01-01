8f769efab3f64ffafd96be9d005dab0ae948c54f
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-01 17:56:26 +0530

    Added : Program for Check Employee is present or absent

--- src/com/blz/day9/EmpWage.java
+++ src/com/blz/day9/EmpWage.java
@@ -1,10 +1,28 @@
 package com.blz.day9;
 
+import java.util.Random;
+
+class utilityEmpWage{
+    Random rand = new Random();
+    public int isPresent = 1;
+    public int isAbsent = 2;
+
+    public void present() {
+        int check = rand.nextInt(2)+1;
+        if (check == isAbsent) {
+            System.out.println("Employee is absent.");
+        }
+        else {
+            System.out.println("Employee is present.");
+        }
+    }
+}
 public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation");
-
+		utilityEmpWage empWage = new utilityEmpWage();
+        empWage.present();
 	}
 
 }
