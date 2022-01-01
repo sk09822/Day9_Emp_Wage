717679bf5fc32dc534424997184fe591f5b2a6a1
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-01 18:05:57 +0530

    Added : Program for Employee is Part time

--- src/com/blz/day9/EmpWage.java
+++ src/com/blz/day9/EmpWage.java
@@ -4,28 +4,32 @@ import java.util.Random;
 
 class utilityEmpWage {
 	Random rand = new Random();
-	public int isPresent = 1;
-	public int isAbsent = 2;
-	public int wagePerHr = 20;
-	public int fullDayHr = 8;
-	int check = rand.nextInt(2) + 1;
+    public int isPresent = 1;
+    public int isAbsent = 2;
+    public int isPartTime = 3;
+    public int wagePerHr = 20;
+    public int fullDayHr = 8;
+    public int partTimeHr = 4;
+    public int dailyWage ;
 
-	public boolean present() {
-		if (check == isAbsent) {
-			System.out.println("Employee is absent.");
-			return false;
-		} else {
-			System.out.println("Employee is present.");
-			return true;
-		}
-	}
 
-	public void dailyWage() {
-		if (present()) {
-			int dailyWage = wagePerHr * fullDayHr;
-			System.out.println("Wage of the day of employee is : " + dailyWage);
-		}
-	}
+    public void present() {
+        int check = rand.nextInt(3)+1;
+        if (check == isAbsent) {
+            System.out.println("Employee is absent.");
+
+        }
+        else if (check == isPresent) {
+            System.out.println("Employee is present.");
+            dailyWage = wagePerHr*fullDayHr;
+            System.out.println("Wage of the day is : " + dailyWage) ;
+        }
+        else {
+            System.out.println("Employee is present part time .");
+            dailyWage = wagePerHr*partTimeHr;
+            System.out.println("Wage of the day is " + dailyWage);
+        }
+    }
 }
 
 public class EmpWage {
